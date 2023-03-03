23a0be7d1cc4734038aa063e8c88b5703b3664bb
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-03-03 14:57:42 -0700

    Added state var to track voting power spent

--- contracts/TokenizedBallot.sol
+++ contracts/TokenizedBallot.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 interface IMyToken {
-    function getPastVotes(address account, uint256 blockNumber) external;
+    function getPastVotes(address account, uint256 blockNumber) external view returns (uint256);
 }
 
 contract Ballot {
@@ -11,11 +11,15 @@ contract Ballot {
         uint voteCount; 
     }
 
+    mapping(address => uint256) public votingPowerSpent;
+
     uint256 public targetBlockNumber;
     IMyToken public tokenContract;
     Proposal[] public proposals;
+
     constructor(bytes32[] memory proposalNames, address _tokenContract, uint256 _targetBlockNumber) {
         tokenContract = IMyToken(_tokenContract);
+        targetBlockNumber = _targetBlockNumber;
         for (uint i = 0; i < proposalNames.length; i++) {
             proposals.push(Proposal({ name: proposalNames[i], voteCount: 0 }));
         }
@@ -23,12 +27,13 @@ contract Ballot {
 
     function vote(uint proposal, uint256 amount) external {
         require(votingPower(msg.sender) >= amount);
+        votingPowerSpent[msg.sender] += amount;
         proposals[proposal].voteCount += amount; 
     }
 
     function votingPower(address account) public view returns (uint256) {
         // performing an external call to get the voting power
-        return tokenContract.getPastVotes(account, blockNumber);
+        return tokenContract.getPastVotes(account, targetBlockNumber) - votingPowerSpent[account];
     }
 
     function winningProposal() public view
