a917c6b79bc4c24bad7accd77818f9c3bd8f6935
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-03-03 19:59:57 -0700

    Added minting of tokens.

--- scripts/ERC20Votes.ts
+++ scripts/ERC20Votes.ts
@@ -1,11 +1,20 @@
 import { MyToken__factory } from "../typechain-types";
 import { ethers } from "hardhat";
 
+const MINT_VALUE = ethers.utils.parseEther("10");
+
 async function main () {
     const [deployer, account1, account2] = await ethers.getSigners();
-    const contractFactory = new MyToken__factory;
-    // Deploy the contrat
+    // Deploy the contract
+    const contractFactory = new MyToken__factory(deployer);
+    const contract = await contractFactory.deploy();
+    const deployTransactionReceipt = await contract.deployTransaction.wait();
+    console.log(`The Tokenized Vote Contract was deployed at the block ${deployTransactionReceipt.blockNumber}`);
     // Mint some tokens
+    const mintTx = await contract.mint(account1.address, MINT_VALUE)
+    const mintTxReceipt = await mintTx.wait();
+    console.log(`Tokens minted for ${account1.address} at block ${mintTxReceipt.blockNumber}`);
+
     // Check voting power
 }
 
