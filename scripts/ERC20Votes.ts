ea4bf02f0e9c3322cfe15cf69bfd429748a22dc6
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-03-03 19:41:34 -0700

    Destructured accounts to use for scripts.

--- scripts/ERC20Votes.ts
+++ scripts/ERC20Votes.ts
@@ -1,5 +1,12 @@
+import { MyToken__factory } from "../typechain-types";
+import { ethers } from "hardhat";
+
 async function main () {
-    console.log("Testing!");
+    const [deployer, account1, account2] = await ethers.getSigners();
+    const contractFactory = new MyToken__factory;
+    // Deploy the contrat
+    // Mint some tokens
+    // Check voting power
 }
 
 main().catch((error) => {
