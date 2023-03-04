819a9a3e3b5d2ec5f3dbd2a6d0301c053102202d
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-03-03 20:47:55 -0700

    Minted tokens for account 2.

--- scripts/ERC20Votes.ts
+++ scripts/ERC20Votes.ts
@@ -10,7 +10,7 @@ async function main () {
     const contract: MyToken = await contractFactory.deploy();
     const deployTransactionReceipt = await contract.deployTransaction.wait();
     console.log(`The Tokenized Vote Contract was deployed at the block ${deployTransactionReceipt.blockNumber}`);
-    // Mint some tokens
+    // Mint some tokens for account 1
     const mintTx = await contract.mint(account1.address, MINT_VALUE);
     const mintTxReceipt = await mintTx.wait();
     console.log(`Tokens minted for ${account1.address} at block ${mintTxReceipt.blockNumber}`);
@@ -20,12 +20,22 @@ async function main () {
     let votePowerAccount1 = await contract.getVotes(account1.address);
     console.log(`Account 1 has a vote power of ${ethers.utils.formatEther(votePowerAccount1)} units`);
     // Self delegate to create checkpoint and grant voting power (delegates everything we have)
-    const delegateTx = await contract.delegate(account1.address);
+    const delegateTx = await contract.connect(account1).delegate(account1.address);
     const delegateTxReceipt = await delegateTx.wait();
-    console.log(`Tokens delegated for ${account1.address} at block ${delegateTxReceipt.blockNumber}`);
+    console.log(`Tokens delegated from ${account1.address} for ${account1.address} at block ${delegateTxReceipt.blockNumber}, cost of ${delegateTxReceipt.gasUsed} gas units, totalling a transaction cost of ${delegateTxReceipt.gasUsed.mul(delegateTxReceipt.effectiveGasPrice)} Wei (${ethers.utils.formatEther(delegateTxReceipt.gasUsed.mul(delegateTxReceipt.effectiveGasPrice))} ETH)`);
     // Check voting power for the second time, after delegation
     votePowerAccount1 = await contract.getVotes(account1.address);
     console.log(`Account 1 has a vote power of ${ethers.utils.formatEther(votePowerAccount1)} units`);
+    // Mint some tokens for account 2
+    const mintTx2 = await contract.mint(account2.address, MINT_VALUE);
+    const mintTxReceipt2 = await mintTx2.wait();
+    console.log(`Tokens minted for ${account2.address} at block ${mintTxReceipt2.blockNumber}`);
+    const tokenBalanceAccount2 = await contract.balanceOf(account2.address);
+    console.log(`Account 2 has a balance of ${ethers.utils.formatEther(tokenBalanceAccount2)} Vote Tokens`);
+    // What block am I at?
+    const currentBlock = await ethers.provider.getBlock("latest");
+    console.log(`The current block number is ${currentBlock.number}`)
+
 
 }
 
