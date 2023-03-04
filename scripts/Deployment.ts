02b6bc2b30854d83d0666b13f348e6f85ae0c5ca
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-03-04 15:03:58 -0700

    Working depl script

--- scripts/Deployment.ts
+++ scripts/Deployment.ts
@@ -1,5 +1,7 @@
 import { MyToken, MyToken__factory, Ballot__factory } from "../typechain-types";
-import { ethers } from "hardhat";
+import { ethers, Wallet } from 'ethers';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const MINT_VALUE = ethers.utils.parseEther("10");
 
@@ -12,7 +14,17 @@ function convertStringArrayToBytes32(array: string[]) {
   }
 
 async function main () {
+    // gets the goerli provider
+    const provider = ethers.getDefaultProvider("goerli");
 
+    const Hardeep_Pk = process.env.PRIVATE_KEY_HARDEEP;
+    // instantiate wallet for Joshua
+    const Joshua_Pk = process.env.PRIVATE_KEY_JOSHUA;
+    if(!Joshua_Pk || Joshua_Pk.length <= 0) throw new Error("Missing environment: private key for Joshua");
+    const wallet = new ethers.Wallet(Joshua_Pk);
+    console.log(`Connected to the wallet address ${wallet.address}`);
+
+    return;
     // get test accounts
     const [deployer, account1, account2] = await ethers.getSigners();
     // Deploy the contract
@@ -146,3 +158,54 @@ main().catch((error) => {
     console.log(error);
     process.exitCode = 1;
 });
+
+///
+
+// import { ethers, Wallet } from 'ethers';
+// import * as dotenv from 'dotenv';
+// import { Ballot__factory } from '../typechain-types';
+// dotenv.config();
+
+// function convertStringArrayToBytes32(array: string[]) {
+//   const bytes32Array = [];
+//   for (let index = 0; index < array.length; index++) {
+//       bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
+//   }
+//   return bytes32Array;
+// }
+
+// async function main () {
+//   const args = process.argv;
+//   const proposals = args.slice(2);
+//   if (proposals.length <= 0) throw new Error("Missing parameters: proposals");
+
+//   const provider = ethers.getDefaultProvider("goerli");
+//   const mnemonic = process.env.MNEMONIC;
+//   if(!mnemonic || mnemonic.length <= 0) throw new Error("Missing environment: mnemonic seed");
+//   const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+//   console.log(`Connected to the wallet address ${wallet.address}`);
+
+//   const signer = wallet.connect(provider);
+//   const balance = await signer.getBalance();
+//   console.log(`Wallet balance: ${balance} Wei`);
+
+//   console.log("Deploying Ballot contract!");
+//   console.log("Proposals: ");
+//   proposals.forEach((element, index) => {
+//     console.log(`Proposal N. ${index + 1}: ${element}`);
+//   });
+//   const ballotContractFactory = new Ballot__factory(signer);
+//   console.log("Deploying contract ...");
+
+//   const ballotContract = await ballotContractFactory.deploy(
+//       convertStringArrayToBytes32(proposals)
+//   );
+//   const deployTxReceipt = await ballotContract.deployTransaction.wait();
+//   console.log(`The Ballot contract was deployed at the address ${ballotContract.address}`);
+//   console.log({deployTxReceipt});
+// }
+
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
